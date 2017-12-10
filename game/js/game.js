f8ecc1c6829e5dfcc3bfcd364323d822ee6d1a03
Krzysztof <lamuerte@op.pl>
2017-12-10 15:49:53 +0100

    animated pot added

--- game/js/game.js
+++ game/js/game.js
@@ -1,3 +1,22 @@
 function initGame() {
+  let $newPot = $('<div class="pot" id="pot"></div>').css({
+    'bottom': '0px',
+    'left': `${(window.innerWidth / 2 - 50)}px`
+  }).appendTo('#game-field');
 
+  $(document).keydown(function (e) {
+    let pos = parseInt(($newPot).css('left'));
+    if (e.keyCode === 37) {
+      $($newPot).css({
+        'left': `${pos - 150}px`
+      })
+    }
+    if (e.keyCode === 39) {
+      $($newPot).css({
+        'left': `${pos + 150}px`
+      })
+    }
+    e.preventDefault();
+  });
 }
+
