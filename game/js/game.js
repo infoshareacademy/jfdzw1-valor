147aee4a5f209c56d251b2030028bc9c80559579
Krzysztof <lamuerte@op.pl>
2017-12-15 19:34:00 +0100

    filed update - not responsive

--- game/js/game.js
+++ game/js/game.js
@@ -1,25 +1,34 @@
 function initGame() {
   let $newPot = $('<div class="pot" id="pot"></div>').css({
-    'left': `${(window.innerWidth / 2 - 50)}px`
+    'left': `385px`
   }).appendTo('#game-field');
 
   $(document).keydown(function (e) {
     let pos = parseInt(($newPot).css('left'));
     if (e.keyCode === 37) {
-      pos -= 100;
+      pos -= 75;
       (pos < 10) ? pos =10 : pos;
       $($newPot).css({
         'left': `${pos}px`
       })
     }
     if (e.keyCode === 39) {
-      pos += 100;
-      (pos > (window.innerWidth - 110)) ? pos = (window.innerWidth - 130) : pos;
+      pos += 75;
+      (pos > 760) ? pos = 760 : pos;
       $($newPot).css({
         'left': `${pos}px`
       })
     }
     e.preventDefault();
   });
+
+  setInterval(function () {
+    let $height = 1;
+    let $newCoin = $('<div class="coin-box" id="coin-box"></div>').css({
+      'top': `${$height*50}px`,
+      'left': `${Math.round(Math.random() * 775)}px`
+    }).appendTo('#game-field');
+  }, 1000);
+
 }
 
